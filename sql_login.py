31a7d950d6ae335b70b66be578a96459e65137dc
Adrian Koszałka <70269475+AdrianKoszalka@users.noreply.github.com>
2020-12-16 11:55:05 +0100

    Delete sql_login.py
    unexpected file

--- sql_login.py
+++
@@ -1,6 +0,0 @@
-class Login(object):
-    def __init__(self):
-        self.hostname = 'localhost',
-        self.username = 'root',
-        self.password = 'mypass',
-        self.database = 'workers'  
